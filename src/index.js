978de019357d80ca7061da28a441f4e6851c0886
Kalixer <al.caceres.ag72@gmail.com>
2022-05-29 15:16:17 -0400

    Responsive implement

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import '../styles/main.css'
 
 const main = document.getElementById('main');
 
+const jscript = () => console.log("ola") 
 
 
 
 
 
+
+
+/*
+
+    1) Alinear las opciones (Checked)
+    2) Hacer funcionar el boton
+        a) console.log
+    3) Arreglar en css
+        a) min-width de los bloques
+        b) crear mas columnas
+*/
