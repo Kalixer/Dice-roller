923f65d3df173a34fc1dc5b20e45941b4d5f86d0
Kalixer <al.caceres.ag72@gmail.com>
2023-03-14 09:54:59 -0300

    Start of the experiment. You'll have to create a canvas for each dice and draw on it the dots

---
+++ assets/dices.js
@@ -0,0 +1,13 @@
+const dice_canva = `<canvas id="dice_structure" width="150" height="150"></canvas>` // this is the structure for the dice on HTML
+
+let d = document.getElementById('dice_structure')
+
+
+// const dices = [
+//     one = 
+// ]
+
+
+
+
+// inserta los canvas en el DOM principal y despues los dibujas/pruebas en este archivo
