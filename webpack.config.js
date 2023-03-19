8d65950017500409f5e720dd4a096c47116c1f2c
Kalixer <al.caceres.ag72@gmail.com>
2023-03-19 16:23:59 -0300

    aliases added on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
     mode: 'development',
     resolve: {
         extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, '/src/components/'),
+            '@containers': path.resolve(__dirname, '/src/containers/'),
+            '@pages': path.resolve(__dirname, '/src/pages/'),
+            '@routes': path.resolve(__dirname, '/src/routes/'),
+            '@styles': path.resolve(__dirname, '/styles/'),
+            '@assets': path.resolve(__dirname, '/assets/')
+        }
     },
     module: {
         rules: [
