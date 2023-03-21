9f8b224778788d5849cc6a9f901e41fae1decee0
Kalixer <al.caceres.ag72@gmail.com>
2023-03-21 18:10:59 -0300

    Dices working properly on /normal and /role. Now I have to make de buttons on the /home page works

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,77 +1,91 @@
 import React from "react";
-import dices from '@assets/dices.js'
-import '@styles/global.css'
-import '@styles/dados.css'
+import dices from '@assets/dices.jsx';
+import '@styles/Resultado.scss';
 
+function randomNumber(min, max){
+    const numero_r = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return numero_r;
+}
+
+const number = typeof(0)
 
 const Resultado = () => {
     const dice_A = document.getElementById('dados_cantidad');
+    const dice_B = document.getElementById('dados_rango');
+    const rango = parseInt(dice_B.value)
+    
+    const dice_amount = parseInt(dice_A.value);
+    console.log(typeof(dice_amount))
+    console.log(dice_amount)
 
-    const result_text = document.getElementById('result');
-    const output = document.getElementById('output');
+    const output = [];
 
-    const boton = document.getElementById("boton")
-    boton.addEventListener('click', throwDice)
+    if(rango <= 6){    
+        console.log(-6)
+        for(let i = 0; i < dice_amount; i++){
+            output.push(dices[randomNumber(0, 5)])
+        }
 
-    function throwDice()
-    {
-        // This is for, when you want to throw again it reloads the text 'These are the results'
-        result_text.innerHTML = ``
+    } else {
+        console.log(+6)
 
-
-        let diceAmount = dice_A.value;
-        let diceRange = dice_R.value;
-
-        let resultado;
-        let loader = `
-            <div class="loader" id="loader">
-                <p>Loading...<p>
-            </div>
-        `
-
-        output.innerHTML = loader
-
-        setTimeout(() => {
-            output.innerHTML = ``
-
-            for(let i = 0; i < diceAmount; i++) {
-                resultado = randomNumber(1, diceRange);
-
-                switch (resultado){
-                    case 1: output.innerHTML += `${dices[0]}`
-                    break;
-                    case 2: output.innerHTML += `${dices[1]}`
-                    break;
-                    case 3: output.innerHTML += `${dices[2]}`
-                    break;
-                    case 4: output.innerHTML += `${dices[3]}`
-                    break;
-                    case 5: output.innerHTML += `${dices[4]}`
-                    break;
-                    case 6: output.innerHTML += `${dices[5]}`
-                    break;
-
-                }
-
-            }
-            result_text.innerHTML = `
-            &#128071; These are the results &#128071;
-            `
-        }, 100)
+        for(let i = 0; i < dice_amount; i++){
+            output.push(<div className="dado">{randomNumber(1, rango)}</div>)
+        }
+        //;
     }
 
-    function randomNumber(min, max){
-        const numero_r = Math.floor(Math.random() * (max - min + 1)) + min;
-
-        return numero_r;
-    }
+    
     
     return (
         <div id="main">
-            <p className="result" id="result">{result_text}</p>
-            <div className="output" id="output"></div>
+            <p className="result" id="result">&#128071; These are the results &#128071;</p>
+            <div className="output" id="output">{output}</div>
         </div>
     );
 }
 
 export default Resultado;
+
+
+
+// const result_text = document.getElementById('result');
+// const output = document.getElementById('output');
+
+// let resultado;
+// let loader = `
+//     <div class="loader" id="loader">
+//         <p>Loading...<p>
+//     </div>
+// `
+
+// output = loader
+
+// setTimeout(() => {
+//     output = ``
+
+//     for(let i = 0; i < diceAmount; i++) {
+//         resultado = randomNumber(1, diceRange);
+
+//         switch (resultado){
+//             case 1: output += `${dices[0]}`
+//             break;
+//             case 2: output += `${dices[1]}`
+//             break;
+//             case 3: output += `${dices[2]}`
+//             break;
+//             case 4: output += `${dices[3]}`
+//             break;
+//             case 5: output += `${dices[4]}`
+//             break;
+//             case 6: output += `${dices[5]}`
+//             break;
+
+//         }
+
+//     }
+//     result_text = `
+//     
+//     `
+// }, 100)
--- src/pages/RoleDice.jsx
+++ src/pages/RoleDice.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
+import Resultado from '../components/Resultado';
+import '@styles/RoleDice.scss';
 
-const MiComponente = () => {
-  const [showComponent, setShowComponent] = useState(false);
+const NormalDice = () => {
+    const [showComponent, setShowComponent] = useState(false); // comienza en estado false
 
-  const handleClick = () => {
-    setShowComponent(!showComponent);
-  }
+    const handleClick = () => {
+      setShowComponent(!showComponent); // Al hacer click cambia el estado al hacerle un NOT al estado anterior
+    }
 
-  return (
-    <div>
-      <button onClick={handleClick}>Mostrar/Ocultar componente</button>
-      {showComponent && {resultado}}
-    </div>
-  );
+    return (
+        <>
+            <div className="encabezado">
+                <h1>Role Dices</h1>
+                <p className="e-paragraph">Choose how many dices you want</p>
+            </div>
+            <div className="input">
+                <div className="options">
+                    <div className="dices"> 
+                        <p>How many dices</p>
+                        <input type="text" id="dados_cantidad" />
+                    </div>
+                    <div className="range">
+                        <p>How many sides</p>
+                        <input type="text" id="dados_rango" />
+                    </div>
+                </div>
+                <button type="button" id="boton" onClick={handleClick}>Throw dices</button>
+            </div>
+            {showComponent && <Resultado />}
+        </>
+    )
 }
 
-export default MiComponente;
+export default NormalDice;
