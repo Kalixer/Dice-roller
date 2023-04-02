28ad45e1bceb7a05e20a72e2b8a6a3d9edcecb43
Kalixer <al.caceres.ag72@gmail.com>
2023-04-02 16:24:19 -0400

    The buttons are working, but with a detail. The 'Throw' button won't 're-throw' unless you reset the dices

--- src/components/ButtonThrow.jsx
+++ src/components/ButtonThrow.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
-import Resultado from '@components/Resultado'
+import Resultado from '@components/Resultado';
 import '@styles/ButtonThrow.scss';
 
 
 const ButtonThrow = () => {
-    const [showComponent, setShowComponent] = useState(false); // comienza en estado false
-    const [messageState, setMessageState] = useState(false);
-
-    let message;
-    if(messageState){
-        message = 'Throw dices'
-    } else {
-        message = 'Again?'
-    }
+    const [showComponent, setShowComponent] = useState(false);
 
     const handleThrow = () => {
 
         // Al hacerle el if, se salta el estado true y el resultado se vuelve a lanzar sin borrarse
         if(showComponent == false) { 
-            setShowComponent(showComponent); // Al hacer click cambia el estado al hacerle un NOT al estado anterior
+            setShowComponent(!showComponent); // Al hacer click cambia el estado al hacerle un NOT al estado anterior
         }
         // setMessageState(!messageState) // Al hacer click cambia el estado al hacerle un NOT al estado anterior
       }
