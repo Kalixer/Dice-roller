a92df86e4379fd7a654b500e671e79ac5b96e8a0
Kalixer <al.caceres.ag72@gmail.com>
2023-04-05 16:55:06 -0400

    [Upgrade] Now the buttons on the 'Header are on the edges. The language button changes it's color on mouse over (actually it changes the image) with a useState hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import AppContext from '../context/AppContext';
 import { Link } from 'react-router-dom';
+
+import lang_image from '@assets/lang_image.png';
+import lang_image_blue from '@assets/lang_image_blue.png';
 import '@styles/Header.scss'
 
 const Header = () => {
     const { language, changeLanguage } = useContext(AppContext);
+    const [langImage, setlangImage] = useState(lang_image)
 
     const handleClick = () => {
         console.log('The actual language is: ' + language)
     }
-
+    
+    const change_to_blue = (dato) => {
+        setlangImage(lang_image_blue)
+    }
+    
+    const normalize = (data) => {
+        setlangImage(lang_image)
+    }
+    let language_logo = <img src={langImage} alt="" />
+    
     return (
         <nav className="Nav">
             <Link className="button-home" to="/">Home</Link>
-            <button type='button' onClick={handleClick}>language</button>
+            <button 
+                type='button' 
+                className='language-button' 
+                onClick={handleClick}
+                onMouseOver={change_to_blue}
+                onMouseLeave={normalize}
+            >
+             {language_logo}   
+            </button>
         </nav>
     )
 }
